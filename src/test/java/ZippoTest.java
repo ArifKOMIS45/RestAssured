26dbb19c74f796a8cd582e4703bc6745c21dc321
ArifKOMIS45 <86622904+ArifKOMIS45@users.noreply.github.com>
2021-08-24 21:20:28 +0300

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class ZippoTest {
+    @Test
+    public void test()
+    {
+        given()//hazirlik islemeri
+                .when()
+                //link ve aksiyon islemleri
+
+                .then()
+        //test ve extract islemleri
+        ;
+    }
+
+}
