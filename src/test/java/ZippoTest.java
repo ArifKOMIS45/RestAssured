8652aed83c9a8fa18c655ae2c19314079b66c5c3
ArifKOMIS45 <86622904+ArifKOMIS45@users.noreply.github.com>
2021-08-24 22:20:37 +0300

    Some Test Added

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,18 +1,43 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+
 public class ZippoTest {
     @Test
-    public void test()
-    {
+    public void test() {
         given()//hazirlik islemeri
-                .when()
-                //link ve aksiyon islemleri
+
+                .when()//link ve aksiyon islemleri
+
 
                 .then()
         //test ve extract islemleri
         ;
     }
+
+    @Test
+    public void statusCodeTest() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()// log.all-->butun response gosterir
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void contentTextType() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+        ;
+
+    }
+
 
 }
