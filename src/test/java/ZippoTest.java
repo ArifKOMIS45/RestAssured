47514275498f41de2e715c6158dae16fb6d650a6
ArifKOMIS45 <86622904+ArifKOMIS45@users.noreply.github.com>
2021-08-26 22:51:32 +0300

    More Test Added3

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,10 +1,25 @@
+import POJO.Location;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.util.Asserts;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
+    private ResponseSpecification responseSpecification;
+    private RequestSpecification requestSpecification;
+
     @Test
     public void test() {
         given()//hazirlik islemeri
@@ -85,7 +100,7 @@ public class ZippoTest {
                 .get("http://api.zippopotam.us/us/90210")
                 .then()
                 .log().body()
-                .body("places[0].'place name'", equalTo("Beverly Hills1"))
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
                 .statusCode(200)
         ;
 
@@ -158,10 +173,11 @@ public class ZippoTest {
         ;
 
     }
+
     @Test
     public void pathParamTest2() {
-        String country="us";
-        String zipcode="90210";
+        String country = "us";
+        String zipcode = "90210";
         given()
                 .pathParam("country", country)
                 .pathParam("zipcode", zipcode)
@@ -176,53 +192,172 @@ public class ZippoTest {
         ;
 
     }
+
     @Test
     public void pathParamTest3() {
-        String country="us";
-        for (int i = 90210; i <90220 ; i++) {
+        String country = "us";
+        for (int i = 90210; i < 90220; i++) {
 
-        String zipcode=String.valueOf(i);
-        given()
-                .pathParam("country", country)
-                .pathParam("zipcode", zipcode)
-                //.pathParam("zipcode", i)
-                .log().uri()
+            String zipcode = String.valueOf(i);
+            given()
+                    .pathParam("country", country)
+                    .pathParam("zipcode", zipcode)
+                    //.pathParam("zipcode", i)
+                    .log().uri()
 
-                .when()
-                .get("http://api.zippopotam.us/{country}/{zipcode}")
-                .then()
-                .log().body()
-               .body("places", hasSize(1))
+                    .when()
+                    .get("http://api.zippopotam.us/{country}/{zipcode}")
+                    .then()
+                    .log().body()
+                    .body("places", hasSize(1))
 
-        ;
+            ;
 
-    }}
+        }
+    }
 
-//https://gorest.co.in/public/v1/users?page=1
+    //https://gorest.co.in/public/v1/users?page=1
     @Test
-    public void queryParamTest(){
+    public void queryParamTest() {
         given()
-                .param("page",1)//boyle yapinca param ile kendisi? mark verip parametre ekliyor
+                .param("page", 1)//boyle yapinca param ile kendisi? mark verip parametre ekliyor
                 .log().uri()
                 .when()
                 .get("https://gorest.co.in/public/v1/users")
                 .then()
-        .body("meta.pagination.page",equalTo(1))
+                .body("meta.pagination.page", equalTo(1))
         ;
     }
 
     @Test
-    public void queryParamTest2(){
-        for (int i = 1; i <10 ; i++) {
+    public void queryParamTest2() {
+        for (int i = 1; i < 10; i++) {
+            given()
+                    .param("page", i)//boyle yapinca param ile kendisi? mark verip parametre ekliyor
+                    .log().uri()
+                    .when()
+                    .get("https://gorest.co.in/public/v1/users")
+                    .then()
+                    .body("meta.pagination.page", equalTo(i))
+            ;
+        }
+    }
+
+    @BeforeClass
+    public void setup() {
+        baseURI = "http://api.zippopotam.us";// Restassure kendi static degiskeni tanimli deger atiyor
+        requestSpecification = new RequestSpecBuilder()
+                .log(LogDetail.URI)
+                .setAccept(ContentType.JSON)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.BODY)
+                .build();
+    }
+
+
+    @Test
+    public void bodyArrayHasSizeTest_baseUriTest_responseSpecification() {
         given()
-                .param("page",i)//boyle yapinca param ile kendisi? mark verip parametre ekliyor
-                .log().uri()
+                .spec(requestSpecification)
                 .when()
-                .get("https://gorest.co.in/public/v1/users")
+                .get("/us/90210")//url nin basinda http yoksa baseUri deki deger otamatik geliyor
                 .then()
-                .body("meta.pagination.page",equalTo(i))
+                .body("places", hasSize(1))//verilen pathdeki listin eleman sayisi
+                .spec(responseSpecification)
         ;
-    }}
+    }
+
+    //jspm extract
+    @Test
+    public void extractingJsonPaty() {
+        String place_name = given()//.spec(requestSpecification)
+                .when()
+                .get("/us/90210")
+                .then()
+                //.spec(responseSpecification)
+                .extract().path("places[0].'place name'")// extract metodu ie gigen ile basliyan satir
+                // disariya bir deger dondurur hale geldi  .. ensonunda extract kullanilmali
+                ;
+        System.out.println("place name= " + place_name);
+
+    }
+
+
+    @Test
+    public void extractingJsonPathInt() {
+        int limit =
+                given()
+                        .param("page", 1)
+                        //.log().uri()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .log().body()
+                        .extract().path("meta.pagination.limit");
+        System.out.println("limit = " + limit);
+    }
+
+    @Test
+    public void extractingJsonPathIntList() {
+        List<Integer> idler =  //data.id demek tum idler demektir
+                given()
+                        .param("page", 1)
+                        //.log().uri()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        //.log().body()
+                        .extract().path("data.id");
+        System.out.println("idler = " + idler);
+    }
+
+    @Test
+    public void extractingJsonPathStringList() {
+        List<String> koyler = given()
+                //.spec(requestSpecification)
+                .when()
+                .get("/tr/01000")
+                .then()
+                //.spec(responseSpecification)
+                .extract().path("places.'place name'")
+                ;
+        System.out.println("koyler = " + koyler);
+        Assert.assertTrue(koyler.contains("Büyükdikili Köyü"));
+
+    }
+
+
+@Test
+    public void extractingJsonPOJO()    // json object
+    {Location location=
+    given()
+            .when()
+            .get("/us/90210")
+                        
+            .then()
+    .extract().as(Location.class)
+    ;
+    System.out.println("location = " + location);
+    System.out.println("location.getCountry() = " + location.getCountry());
+    System.out.println("location = " + location.getPlaces());
+    System.out.println("location.getPlaces().get(0).getPlacename() = " + location.getPlaces().get(0).getPlacename());
+    
+}
+
+
+
+
+
+
+
+
 
 
 
